52a5ee170328b731145f8eab2c8889468934cf53
eboshidori-pen <64635773+eboshidori-pen@users.noreply.github.com>
2023-12-11 14:41:31 +0900

    選択中のアイテム実装へ奮闘した

---
+++ p2hacks2023/Assets/script/BagToSelectButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BagToSelectButton : MonoBehaviour
+{
+    public Button itemButton;
+
+    void Start()
+    {
+        //itemButton.onClick.AddListener(ActivatePanel);
+    }
+
+    void Update()
+    {
+        
+    }
+}
