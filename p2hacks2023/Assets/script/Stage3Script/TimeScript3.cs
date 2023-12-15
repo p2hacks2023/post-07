c9f3d349aa96adf0ecfb254b135ef08bd09daeb2
fumika0622 <b1020253@fun.ac.jp>
2023-12-15 19:02:25 +0900

    stage3でテキスト表示中はアニメーションとタイマーがストップ

--- p2hacks2023/Assets/script/Stage3Script/TimeScript3.cs
+++ p2hacks2023/Assets/script/Stage3Script/TimeScript3.cs
@@ -22,13 +22,19 @@ public class TimeScript3 : MonoBehaviour
     //　追加時間（秒）
 	[SerializeField]
 	private float addseconds;
+
+	//テキスト表示中かどうかのフラグ
+	public static bool isText = false;
     
     // Start is called before the first frame update
     void Start()
     {
         totalTime3 = TimeScript2.totalTime2 + addseconds;
 		oldSeconds = 0f;
+		minute = (int) totalTime3 / 60;
+		seconds = totalTime3 - minute * 60;
 		timerText = GetComponentInChildren<Text>();
+		timerText.text = minute.ToString("00") + ":" + ((int) seconds).ToString("00");
     }
 
     // Update is called once per frame
@@ -38,20 +44,26 @@ public class TimeScript3 : MonoBehaviour
 		if (totalTime3 <= 0f) {
 			return;
 		}
-		//　一旦トータルの制限時間を計測；
-		//totalTime = minute * 60 + seconds;
-		totalTime3 -= Time.deltaTime;
+
+		//テキスト表示中でなければ
+		if(isText == false)
+		{
+			//　一旦トータルの制限時間を計測；
+			//totalTime = minute * 60 + seconds;
+			totalTime3 -= Time.deltaTime;
  
-		//　再設定
-		minute = (int) totalTime3 / 60;
-		seconds = totalTime3 - minute * 60;
+			//　再設定
+			minute = (int) totalTime3 / 60;
+			seconds = totalTime3 - minute * 60;
         
  
-		//　タイマー表示用UIテキストに時間を表示する
-		if((int)seconds != (int)oldSeconds) {
+			//　タイマー表示用UIテキストに時間を表示する
+			if((int)seconds != (int)oldSeconds) {
 			timerText.text = minute.ToString("00") + ":" + ((int) seconds).ToString("00");
+			}	
+			oldSeconds = seconds;
 		}
-		oldSeconds = seconds;
+		
 		//　制限時間以下になったらゲームオーバーパネルを表示する
 		if(totalTime3 <= 0f) {
 			GameOverPanel.SetActive(true);
