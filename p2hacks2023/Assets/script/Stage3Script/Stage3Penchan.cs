b006d26852e43ce49da279db8789986da2228344
fumika0622 <b1020253@fun.ac.jp>
2023-12-15 15:53:22 +0900

    stage3のペンちゃんビジョンを修正

--- p2hacks2023/Assets/script/Stage3Script/Stage3Penchan.cs
+++ p2hacks2023/Assets/script/Stage3Script/Stage3Penchan.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Stage3Penchan : MonoBehaviour
 {
     public GameObject PenchanPanel;
+    public GameObject FrontPanel;
+    public GameObject ItemTextPanel10_1;
+    public GameObject ItemTextPanel10_2;
+    public GameObject ItemTextPanel50_1;
+    public GameObject ItemTextPanel50_2;
+    public GameObject ItemTextPanel100_1;
+    public GameObject ItemTextPanelIce;
     
     void Start()
     {
@@ -15,6 +22,13 @@ public class Stage3Penchan : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.F) && PenchanPanel.activeSelf == false){
             PenchanPanel.SetActive(true);
+            FrontPanel.SetActive(false);
+            ItemTextPanel10_1.SetActive(true);
+            ItemTextPanel10_2.SetActive(true);
+            ItemTextPanel50_1.SetActive(true);
+            ItemTextPanel50_2.SetActive(true);
+            ItemTextPanel100_1.SetActive(true);
+            ItemTextPanelIce.SetActive(true);
 
             //〇秒後に呼び出す
             Invoke(nameof(DelayMethod), 3.5f);
@@ -24,6 +38,13 @@ public class Stage3Penchan : MonoBehaviour
     void DelayMethod()
     {
         PenchanPanel.SetActive(false);
+        FrontPanel.SetActive(true);
+        ItemTextPanel10_1.SetActive(false);
+        ItemTextPanel10_2.SetActive(false);
+        ItemTextPanel50_1.SetActive(false);
+        ItemTextPanel50_2.SetActive(false);
+        ItemTextPanel100_1.SetActive(false);
+        ItemTextPanelIce.SetActive(false);
     }
 
     private void OnDestroy()
