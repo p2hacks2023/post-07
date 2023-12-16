6a230fd26b15d68961414452f061dfb2ad035000
eboshidori-pen <64635773+eboshidori-pen@users.noreply.github.com>
2023-12-16 13:07:01 +0900

    タイトル基本機能実装

---
+++ p2hacks2023/Assets/script/TitleScript/FadeChangeScene.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeChangeScene : MonoBehaviour
+{
+    [SerializeField] private GameObject FadeImage;
+    public string sceneNameString;
+    Color color;
+
+    void Start()
+    {
+        color = FadeImage.GetComponent<SpriteRenderer>().color;
+        color.a = 0f;
+        FadeImage.GetComponent<SpriteRenderer>().color = color;
+    }
+/*
+    public void OnClick(){
+
+        FadeControll();
+
+
+        //if(color.a <= 0.45f){
+            
+        //}
+    }*/
+
+    void Update(){
+        //while(color.a <= 1f){
+            color.a += 0.008f;
+            FadeImage.GetComponent<SpriteRenderer>().color = color;
+        //}
+        if(color.a >= 1f){
+            LoadScene(sceneNameString);
+        }
+
+        //LoadScene(sceneNameString);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
